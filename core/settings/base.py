f2f68a1436bf456c8ec81ae1f2b0665d327b0dfb
mrJumbash <jumbash00@gmail.com>
2023-05-03 21:53:37 +0600

    email enving

--- core/settings/base.py
+++ core/settings/base.py
@@ -121,8 +121,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "sadyrbaevkubanych@gmail.com"
-EMAIL_HOST_PASSWORD = "teytqomsrrezlifm"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 
 SIMPLE_JWT = {
